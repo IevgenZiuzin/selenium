685affaf07b7303b567f227a99a106b9ecf5379b
ievhen <zeugenz@gmail.com>
2023-04-16 23:05:12 +0300

    get_search_results function added

---
+++ main.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+URL = 'https://www.google.com/'
+PREfIX = 'search?q='
+
+
+driver = webdriver.Edge()
+
+
+def get_search_results(prompts):
+    suffix = '+'.join(prompts)
+    search_query = URL + PREfIX + suffix
+
+    try:
+        driver.get(URL)
+        assert "Google" in driver.title
+        elem = driver.find_element(By.CSS_SELECTOR, "button:nth-of-type(2)")
+        elem.click()
+    except:
+        driver.get(search_query)
+
+    driver.implicitly_wait(5)
+
+    results_wrapper = driver.find_element(By.CSS_SELECTOR, "#search")
+    results = results_wrapper.find_elements(By.TAG_NAME, "h3")
+    for i, result in enumerate(results[:5], 1):
+        title = result.get_attribute('innerHTML')
+        url = result.find_element(By.XPATH, "..").get_attribute('href')
+        if url:
+            print(title)
+            print(url)
+            print('\n')
+    driver.close()
+
+
+if __name__ == '__main__':
+    get_search_results(['python', 'developer'])
